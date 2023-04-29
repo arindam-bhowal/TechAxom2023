ec29d45988693b4c513494d9a0138a9dc55d8def
Arindam <arindambhowal10@gmail.com>
2023-04-29 12:29:01 +0530

    tried to add OCR feature

--- Extension/background.js
+++ Extension/background.js
@@ -60,42 +60,42 @@ chrome.webNavigation.onHistoryStateUpdated.addListener(function(data) {
 
 
 
-// var id = 100;
+var id = 100;
 
 
 
 
-// chrome.browserAction.onClicked.addListener(function() {
+chrome.browserAction.onClicked.addListener(function() {
 
-//     chrome.tabs.captureVisibleTab(function(screenshotUrl) {
-//         var viewTabUrl = chrome.extension.getURL('screenshot.html?id=' + id++)
-//         var targetId = null;
+    chrome.tabs.captureVisibleTab(function(screenshotUrl) {
+        var viewTabUrl = chrome.extension.getURL('screenshot.html?id=' + id++)
+        var targetId = null;
 
-//         console.log(screenshotUrl);
+        console.log(screenshotUrl);
 
-//         chrome.tabs.onUpdated.addListener(function listener(tabId, changedProps) {
+        chrome.tabs.onUpdated.addListener(function listener(tabId, changedProps) {
         
-//             if (tabId != targetId || changedProps.status != "complete")
-//                 return;
-//             chrome.tabs.onUpdated.removeListener(listener);
-//             var views = chrome.extension.getViews();
-//             for (var i = 0; i < views.length; i++) {
-//                 var view = views[i];
-//                 if (view.location.href == viewTabUrl) {
-//                     view.setScreenshotUrl(screenshotUrl);
-//                     break;
-//                 }
-//             }
-//         });
+            if (tabId != targetId || changedProps.status != "complete")
+                return;
+            chrome.tabs.onUpdated.removeListener(listener);
+            var views = chrome.extension.getViews();
+            for (var i = 0; i < views.length; i++) {
+                var view = views[i];
+                if (view.location.href == viewTabUrl) {
+                    view.setScreenshotUrl(screenshotUrl);
+                    break;
+                }
+            }
+        });
 
-//         chrome.tabs.create({
-//             url: viewTabUrl
-//         }, function(tab) {
-//             targetId = tab.id;
-//         });
-//     });
+        chrome.tabs.create({
+            url: viewTabUrl
+        }, function(tab) {
+            targetId = tab.id;
+        });
+    });
 
-// });
+});
 
 
 
